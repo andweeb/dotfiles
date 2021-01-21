4c5b87690627a7e8e964d176162d31d17706135e
Andrew Kwon <andrewshky@gmail.com>
2021-01-21 10:15:54 -0800

    Remove unused aliases

--- zsh/.aliases.zsh
+++ zsh/.aliases.zsh
@@ -1,11 +1,9 @@
-alias q='exit'
-alias l='ls -la'
-alias rm='rm -irv'
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
-alias cwd='pwd | pbcopy'
 
+alias rm='rm -irv'
+alias cwd='pwd | pbcopy'
 alias bat='bat --theme="gruvbox-dark" --style=header,changes'
 
 # Alias if neovim is installed
