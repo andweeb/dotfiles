f5fd01add33e15243fe08a61320127bcaf812914
Andrew Kwon <andrewshky@gmail.com>
2021-02-02 00:02:33 -0800

    Add Flow and TypeScript language servers

--- vim/lsp.lua
+++ vim/lsp.lua
@@ -1,6 +1,7 @@
 local lspconfig = require("lspconfig")
 local completion = require("completion")
 
+-- Lua
 local lsp_install_path = vim.fn.stdpath('cache')..'/lspconfig'
 local sumneko_root_path = lsp_install_path..'/sumneko_lua/lua-language-server'
 local sumneko_bin = sumneko_root_path.."/bin/macOS/lua-language-server"
@@ -14,5 +15,16 @@ lspconfig.sumneko_lua.setup {
                 globals = { "hs", "spoon", "vim" },
             },
         }
-    }
+    },
+}
+
+-- Flow
+lspconfig.flow.setup {
+    on_attach = completion.on_attach,
+}
+
+-- TypeScript
+lspconfig.tsserver.setup {
+    on_attach = completion.on_attach,
+    filetypes = { "typescript", "typescriptreact", "typescript.tsx" }
 }
