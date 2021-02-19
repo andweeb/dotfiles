449e073bc13ef3b583bc5d8d4261bad7e3336b85
Andrew Kwon <andrewshky@gmail.com>
2021-02-19 14:18:00 -0800

    Add custom LSP attach func with default bindings

--- vim/lsp.lua
+++ vim/lsp.lua
@@ -1,13 +1,37 @@
 local lspconfig = require("lspconfig")
 local completion = require("completion")
 
+local function on_attach(client, bufnr)
+    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+    local opts = { noremap=true, silent=true }
+    buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+    buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+    buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+
+    completion.on_attach(client, bufnr)
+end
+
 -- Lua
 local lsp_install_path = vim.fn.stdpath('cache')..'/lspconfig'
 local sumneko_root_path = lsp_install_path..'/sumneko_lua/lua-language-server'
 local sumneko_bin = sumneko_root_path.."/bin/macOS/lua-language-server"
 
 lspconfig.sumneko_lua.setup {
-    on_attach = completion.on_attach,
+    on_attach = on_attach,
     cmd = { sumneko_bin, "-E", sumneko_root_path.."/main.lua" },
     settings = {
         Lua = {
@@ -20,11 +44,11 @@ lspconfig.sumneko_lua.setup {
 
 -- Flow
 lspconfig.flow.setup {
-    on_attach = completion.on_attach,
+    on_attach = on_attach,
 }
 
 -- TypeScript
 lspconfig.tsserver.setup {
-    on_attach = completion.on_attach,
+    on_attach = on_attach,
     filetypes = { "typescript", "typescriptreact", "typescript.tsx" }
 }
