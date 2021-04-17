d8fbccbcd512483508ff6c28e21f8d91fbb82b59
Andrew Kwon <andrewshky@gmail.com>
2021-04-17 11:24:03 -0700

    Update zsh aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,10 +5,20 @@ alias ....='cd ../../..'
 alias rm='rm -irv'
 alias cwd='pwd | pbcopy'
 alias bat='bat --theme="gruvbox-dark" --style=header,changes'
-alias update-asdf-nvim-nightly='asdf uninstall neovim nightly && asdf install neovim nightly'
 
-# Alias if neovim is installed
+# Functions for asdf
+function asdf-update-nightly-nvim () {
+    asdf uninstall neovim nightly && \
+        asdf install neovim nightly
+}
+function asdf-add-plugins () {
+    cut -d' ' -f1 .tool-versions | sort \
+      | comm -23 - <(asdf plugin-list | sort) \
+      | join -a1 - <(asdf plugin list all) \
+      | xargs -t -L1 asdf plugin add
+}
+
+# Alias nvim if installed
 if command -v "nvim" > /dev/null; then
     alias vi='nvim'
-    alias vim='nvim'
 fi
