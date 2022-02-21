82096ab574f7abb075654047333f53c5d5dddc73
Andrew Kwon <andrewshky@gmail.com>
2022-02-21 11:14:59 -0800

    Configure mappings for toggleable terminal processes

--- vim/term.lua
+++ vim/term.lua
@@ -1,9 +1,6 @@
 local toggleterm = require("toggleterm")
 local Terminal = require("toggleterm.terminal").Terminal
 
--- Table of terminal program toggles
-_G.TermToggles = {}
-
 -- Setup toggleterm
 toggleterm.setup({
     size = 20,
@@ -16,11 +13,11 @@ toggleterm.setup({
     insert_mappings = true,
     terminal_mappings = true,
     persist_size = true,
-    direction = 'float',
+    direction = "float",
     close_on_exit = true,
     shell = vim.o.shell,
     float_opts = {
-        border = 'single',
+        border = "single",
         winblend = 0,
         highlights = {
             border = "Normal",
@@ -29,33 +26,64 @@ toggleterm.setup({
     }
 })
 
--- Set keymaps in terminal mode
+-- Terminal process toggle mappings keyed by command, used to:
+-- * explicitly map with defined mappings
+-- * override the toggle command to enable toggling out
+_G.toggles = {
+    node = {
+        mapping = "<C-n>",
+        terminal = nil,
+    },
+    nvim = {
+        mapping = "<leader>v",
+        terminal = nil,
+    },
+}
+
+-- Set buffer-specific keymaps depending on the process
 function _G.set_terminal_keymaps()
     local opts = { noremap = true }
-    -- Enter normal mode in terminal mode with leader key and escape
-    vim.api.nvim_buf_set_keymap(0, 't', '<leader><esc>', [[<C-\><C-n>]], opts)
+
+    -- Parse the command from buffer name
+    local current_buf = vim.api.nvim_get_current_buf()
+    local buf_name = vim.api.nvim_buf_get_name(current_buf)
+    local cmd = buf_name:match("term://.*:(.*);.*")
+    local process = _G.toggles[cmd]
+
+
+    -- Override any process toggle command mappings in terminal mode to allow toggling out
+    if process then
+        local toggle_cmd = string.format([[<cmd>lua toggle_terminal_process("%s")<CR>]], cmd)
+        vim.api.nvim_buf_set_keymap(0, "t", process.mapping, toggle_cmd, opts)
+    end
+
+    -- Allow exiting terminal mode with both leader and escape keys
+    vim.api.nvim_buf_set_keymap(0, "t", "<leader><esc>", [[<C-\><C-n>]], opts)
 end
 
--- Helper function to map various terminal programs
-local function map(cmd, keycode)
-    local program = Terminal:new({
+-- Toggle a terminal process by the command name
+function _G.toggle_terminal_process(cmd)
+    local process = _G.toggles[cmd]
+    if not process or not process.terminal then
+        print(string.format("Unable to toggle unexpected terminal process: %s", cmd))
+        return
+    end
+
+    process.terminal:toggle()
+end
+
+-- Add mappings for various terminal process toggles
+for cmd, process in pairs(_G.toggles) do
+    process.terminal = Terminal:new({
         cmd = cmd,
         hidden = true,
         dir = "git_dir",
         direction = "float",
     })
 
-    _G.TermToggles[cmd] = function()
-        program:toggle()
-    end
-
-    local handler = string.format([[<cmd>lua TermToggles["%s"]()<CR>]], cmd)
-    vim.api.nvim_set_keymap("n", keycode, handler, { noremap = true, silent = true })
+    local handler = string.format([[<cmd>lua toggle_terminal_process("%s")<CR>]], cmd)
+    vim.api.nvim_set_keymap("n", process.mapping, handler, { noremap = true, silent = true })
 end
 
--- Add mappings for various terminal program toggles
-map("node", "<leader><C-n>")
-map("nvim", "<leader><C-v>")
-
 -- Apply mappings for the terminal
-vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
